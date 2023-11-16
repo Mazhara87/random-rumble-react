a509e32ae5286ff8aa629fcc67f6f5037b509d4f
Mazhara87 <olgamazhara87@gmail.com>
2023-11-16 15:51:28 +0100

    add partie 4,5

--- src/Components/ButtonCapacity.js
+++ src/Components/ButtonCapacity.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { hitBack, hitMonster } from '../features/fight/fightSlice';
 
 
+const ButtonCapacity = ({ playerId }) => {
+    const dispatch = useDispatch();
 
-class ButtonCapacity extends React.Component {
-     combat = () => {
-        console.log('aie !')
-    }
-    render() {
-        return (
-            <button type="button" onClick={() => this.combat()} className="btn btn-success material-tooltip-main ">
-                hit
-            <i className="fas fa-bomb"></i> 5
-        <i className="fas fa-fire-alt"></i> - 5
+    const combat = () => {
+        dispatch(hitMonster({
+                monsterDecreasePv: 5
+            }));
+            dispatch(hitBack({
+                playerHit: playerId-1,
+                playerDecreasePv: 5
+    }))
+    };
+
+    return (
+        <button
+         type="button" 
+        onClick={() =>combat()} 
+        className="btn btn-success material-tooltip-main "
+        >
+            hit
+        <i className="fas fa-bomb"></i> 5<i className="fas fa-fire-alt"></i> - 5
     </button>
-        )
-    }
-
-}
+    )
+    };
 
 
 
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import fightReducer from '../features/fight/fightSlice'
+
+
+export const store = configureStore({
+    reducer: {
+      fight: fightReducer
+    },
+  })
